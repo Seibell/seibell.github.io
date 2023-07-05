d52382af34082df0e6af557c73ef35214f7e5fc3
Seibell <66213395+Seibell@users.noreply.github.com>
2023-07-05 17:13:37 +0800

    added semi language support but half done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import { Box, useColorMode, Flex, Heading } from "@chakra-ui/react";
+import { Box, useColorMode, Flex, Heading, Select } from "@chakra-ui/react";
+import { useState } from "react";
 import DarkModeButton from './components/DarkModeButton';
 import NavBar from './components/NavBar';
+import { languageData } from './constants/languageData';
 
 function App() {
+  const currentYear = new Date().getFullYear();
+  const [selectedLanguage, setSelectedLanguage] = useState("en");
+
+  const handleLanguageChange = (event) => {
+    setSelectedLanguage(event.target.value);
+  };
+
+  const { greeting, name } = languageData[selectedLanguage];
+
   return (
     <Box overflow="hidden" h="100vh">
       <Flex
@@ -19,10 +30,13 @@ function App() {
           justify="center"
           style={{ minHeight: 'calc(100vh - 40px)', marginTop: '-40px' }}
         >
-          <Heading mb={4} fontSize="3xl">Welcome to My Homepage</Heading>
-
+          <Heading fontSize="4xl">{greeting}</Heading>
+          <Heading mb={4} fontSize="4xl">{name}</Heading>
           <DarkModeButton />
         </Flex>
+        <footer className="footer" style={{ marginTop: '-10px', fontWeight: 'semibold' }}>
+          © {currentYear} Ryan Lim
+        </footer>
       </Flex>
     </Box>
   );
---
+++ src/constants/languageData.js
@@ -0,0 +1,18 @@
+export const languageData = { 
+    en: {
+        greeting: "Nice to meet you",
+        name: "My name is Ryan",
+    },
+    cn: {
+        greeting: "你好",
+        name: "我叫林俊伟",
+    },
+    jp: {
+        greeting: "はじめまして",
+        name: "私の名前はライアンです",
+    },
+    kr: {
+        greeting: "반가워요",
+        name: "나는 라이언"
+    }
+}
